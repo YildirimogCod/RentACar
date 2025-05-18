0ce7ee44cbf9b4b45c1b1f505d4ba812dbd8be53
yildirimogcod <yildirimogcod@gmail.com>
2025-05-19 00:57:25 +0300

    Add project files.

---
+++ Core/RentCar.Domain/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace RentCar.Domain.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Type { get; set; }    
+        public string Fuel { get; set; }
+        public string Color { get; set; }
+        public decimal PricePerDay { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Core/RentCar.Domain/Entities/RentedCar.cs
@@ -0,0 +1,17 @@
+﻿namespace RentCar.Domain.Entities
+{
+    public class RentedCar
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal DamagePrice { get; set; }
+        public bool IsReturned { get; set; }
+        public Car Car { get; set; }
+        public User User { get; set; }
+
+    }
+}
